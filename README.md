1b59e8c1d403033bc5057eae48ab19bf28af28bd
John Cook <john@johncook.me.uk>
2014-11-20 00:22:25 +0000

    Mostly working

---
+++ README.md
@@ -0,0 +1,4 @@
+favicon_generator
+=================
+
+Real Favicon Generator for Drupal. see http://realfavicongenerator.net/ https://www.drupal.org/
---
+++ favicon_generator.js
@@ -0,0 +1,11 @@
+(function($) {
+
+Drupal.behaviors.favicon_generator = {
+  attach: function(context) {
+    $('.colorpicker').farbtastic('#edit-favicon-generator-color');
+  }
+
+};
+
+})(jQuery);
+
