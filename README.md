56d4d7341d1dd35b63195671c395f6ffe6163bc1
John Cook <john@johncook.me.uk>
2014-11-21 10:37:47 +0000

    Fixing codinging standards https://www.drupal.org/node/2378799#comment-9362375

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 favicon_generator
 =================
 
-Real Favicon Generator for Drupal. see http://realfavicongenerator.net/ https://www.drupal.org/
+Real Favicon Generator for Drupal. see http://realfavicongenerator.net/
+https://www.drupal.org/
--- favicon_generator.js
+++ favicon_generator.js
@@ -1,11 +1,13 @@
+/**
+ * @file
+ *
+ * Favicon generator color picker function
+ */
+
 (function($) {
-
-Drupal.behaviors.favicon_generator = {
-  attach: function(context) {
-    $('.colorpicker').farbtastic('#edit-favicon-generator-color');
-  }
-
-};
-
+  Drupal.behaviors.favicon_generator = {
+    attach: function(context) {
+      $('.colorpicker').farbtastic('#edit-favicon-generator-color');
+    }
+  };
 })(jQuery);
-
