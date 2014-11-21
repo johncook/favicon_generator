c07e1b710a93f4153ca6e74be26fbb120fb3fe82
John Cook <john@johncook.me.uk>
2014-11-21 23:06:03 +0000

    Implemented color swatch onClick event

--- favicon_generator.js
+++ favicon_generator.js
@@ -6,8 +6,31 @@
 
 (function($) {
   Drupal.behaviors.favicon_generator = {
-    attach: function(context) {
-      $('.colorpicker').farbtastic('#edit-favicon-generator-color');
+    attach: function(context, settings) {
+      // Attach color wheel behaviour
+      $('.favicon_generator_colorpicker').farbtastic('#edit-favicon-generator-color');
+
+      // Attach onClick event for swatches
+      $('.form-item-favicon-generator-color .color-preset:not(.favicon-generator-color-preset-processed)')
+      .addClass('favicon-generator-color-preset-processed')
+      .click(function() {
+        var color = $(this).css('background-color');
+
+        function rgb2hex(rgb) {
+          if (rgb.search("rgb") == -1)
+            return rgb;
+          else {
+            rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+            function hex(x) {
+              return ("0" + parseInt(x).toString(16)).slice(-2);
+            }
+            return ("#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]));
+          }
+        }
+
+        color = rgb2hex(color);
+        $.farbtastic('.favicon_generator_colorpicker').setColor(color);
+      });
     }
   };
 })(jQuery);
